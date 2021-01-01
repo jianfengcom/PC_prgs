49da4cab2d2c5862fb280a6cc7faf9f07168aaf0
chenjianfeng <chenjianfeng@163.cn>
2021-01-01 13:56:19 +0800

    product-3g - 烟台

--- src/main/resources/cn/pconline/bestactivity/改表.sql
+++ src/main/resources/cn/pconline/bestactivity/改表.sql
@@ -7,3 +7,10 @@ ALTER TABLE `pcbest_account_ecard_share` DROP COLUMN `help_id`;
 
 ALTER TABLE `pcbest_account_ecard_share` ADD COLUMN `share_pid` bigint(11) DEFAULT NULL COMMENT '分享方 - 通行证ID' AFTER `id`;
 ALTER TABLE `pcbest_account_ecard_share` ADD COLUMN `help_pid` bigint(11) DEFAULT NULL COMMENT '助力方 - 通行证ID' AFTER `id`;
+
+
+1.备份 pcbest_account_ecard 和 pcbest_account_ecard_share
+
+2.截断 pcbest_account_ecard 和 pcbest_account_ecard_share
+
+3.执行上面语句
